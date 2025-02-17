4799159928695dee9de09f19b3e9050c09edcd6c
sachin-lakhlan <lakhlansachin0@gmail.com>
2025-02-17 16:07:47 +0530

    tow-section-complete

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+const navbarbtn = () => {
+    document.getElementById("name").classList.toggle("show-navbar")
+    document.getElementById("rotates").classList.toggle("rotate40")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotates-2").classList.toggle("rotate-45")
+    document.body.classList.toggle("overflow-hidden")
+}
+
+$('.my-slider').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true
+            }
+        }
+    ]
+});
